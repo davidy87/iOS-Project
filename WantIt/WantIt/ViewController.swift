824f9a34acbd8bbf9417c3d84d3b87cd8f192b10
David Yoon <dyoon0807@gmail.com>
2020-06-29 23:06:48 -0500

    Project created

---
+++ WantIt/WantIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WantIt
+//
+//  Created by David Yoon on 6/29/20.
+//  Copyright © 2020 David Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
