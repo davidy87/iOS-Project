90fd61ae8248983280dd30421a758ef15f25205d
David Yoon <dyoon0807@gmail.com>
2020-06-30 03:34:20 -0500

    Building storyboard

---
+++ WantIt/WantIt/HomeViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  WantIt
+//
+//  Created by David Yoon on 6/29/20.
+//  Copyright © 2020 David Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+}
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var itemTableView: UITableView!
+    
+    let segueIdentifier = "ItemSegueIdentifier"
+    var items: [Item]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: segueIdentifier,
+            for: indexPath as IndexPath
+        ) as! ItemTableViewCell
+        
+        let item = items[indexPath.row]
+        
+        cell.title.text = item.title
+        cell.price.text = String(item.price)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueIdentifier,
+            let nextVC = segue.destination as? ItemViewController {
+            nextVC.delegate = self
+        }
+    }
+
+
+}
+
---
+++ WantIt/WantIt/ItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ItemViewController.swift
+//  WantIt
+//
+//  Created by David Yoon on 6/30/20.
+//  Copyright © 2020 David Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+    
+    @IBOutlet weak var itemInfoTableView: UITableView!
+    
+    
+    var delegate: UIViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
